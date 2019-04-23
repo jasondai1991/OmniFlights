0e5b000aab0fe84b5619d4b415d78c2897587ffd
jasondai1991 <zhixuan_dai@berkeley.edu>
2019-04-22 18:28:59 -0700

    v0.5

---
+++ js/bar.js
@@ -0,0 +1,344 @@
+var fileName = "../ReadyData/chart2.csv";
+var chartLables = [];
+
+var airline_translation = {"NK":"Spirit",
+                   "WN":"Southwest",
+                   "B6":"Jetblue",
+                   "UA":"United",
+                   "AA":"American",
+                   "DL":"Delta",
+                   "OO":"SkyWest",
+                   "F9":"Frontier",
+                   "EV":"ExpressJet",
+                   "HA":"Hawaiian",
+                   "AS":"Alaska",
+                   "MQ":"AmericanEagle"
+                  };
+
+var listForAll = [],
+    cityMap = {},
+    airlineMap = {},
+    maxVal = {};
+
+for(var i=0; i<144; i++){
+    chartLables.push(i);
+    listForAll.push(0);
+}
+
+d3.csv(fileName, function(error, data) {
+    data.forEach(function(d) {
+        if(d.Type == 'City'){
+            var city = d.Index;
+            cityMap[city] = [];
+            chartLables.forEach(function(field) {
+                var curval = d[field];
+                if(typeof curval === "undefined"){
+                    curval = 0;
+                }
+                cityMap[city].push( +curval );
+                listForAll[field] += Number(curval);
+            });
+            maxVal[city] = Math.max(...cityMap[city]);
+        } else{
+            var airline = airline_translation[d.Index];
+            airlineMap[airline] = [];
+            chartLables.forEach(function(field) {
+                var curval = d[field];
+                if(typeof curval === "undefined"){
+                    curval = 0;
+                }
+                airlineMap[airline].push( +curval );
+            });
+            maxVal[airline] = Math.max(...airlineMap[airline]);
+        }
+    });
+    cityMap.All = listForAll;
+    airlineMap.All = listForAll;
+    maxVal["All"] = Math.max(...listForAll);
+});
+
+var tierdMap = {},
+    tierdFile = "../ReadyData/chart3.csv",
+    tierdMaxVal = {};
+
+d3.csv(tierdFile, function(error, data) {
+    data.forEach(function(d) {
+        var airline = airline_translation[d.airline];
+        if(!(airline in tierdMap)){
+            tierdMap[airline] = {};
+        }
+        var city = d.City;
+        var curMap = tierdMap[airline];
+        curMap[city] = [];
+        chartLables.forEach(function(field) {
+            var curval = d[field];
+            if(typeof curval === "undefined"){
+                curval = 0;
+            }
+            curMap[city].push( +curval );
+        });
+        tierdMap[airline] = curMap;
+        if(!(airline in tierdMaxVal)){
+            tierdMaxVal[airline] = {};
+        }
+        var curMaxVal = tierdMaxVal[airline];
+        curMaxVal[city] = Math.max(...curMap[city]);
+        tierdMaxVal[airline] = curMaxVal;
+        tierdMap[airline].All = airlineMap[airline];
+        tierdMaxVal[airline].All = maxVal[airline];
+    });
+});
+
+function selectAirline(){
+    d3.select("#Both").select("*").remove();
+    var cityBtn = document.getElementById("cityBtn");
+    var bothBtn = document.getElementById("bothBtn");
+    var airlineBtn = document.getElementById("airlineBtn");
+    cityBtn.style.backgroundColor = "#e7e7e7";
+    bothBtn.style.backgroundColor = "#e7e7e7";
+    airlineBtn.style.backgroundColor = "#00BFFF";
+
+    var container1 = d3.select("#vis-container1");
+    var container2 = d3.select("#vis-container2");
+
+    makeVis(container1, airlineMap, maxVal);
+    makeVis(container2, airlineMap, maxVal);
+}
+
+function selectCity(){
+    d3.select("#Both").select("*").remove();
+    var cityBtn = document.getElementById("cityBtn");
+    var bothBtn = document.getElementById("bothBtn");
+    var airlineBtn = document.getElementById("airlineBtn");
+    airlineBtn.style.backgroundColor = "#e7e7e7";
+    bothBtn.style.backgroundColor = "#e7e7e7";
+    cityBtn.style.backgroundColor = "#00BFFF";
+
+    var container1 = d3.select("#vis-container1");
+    var container2 = d3.select("#vis-container2");
+
+    makeVis(container1, cityMap, maxVal);
+    makeVis(container2, cityMap, maxVal);
+}
+
+function selectBoth(){
+    d3.select("#Both").select("*").remove();
+    var cityBtn = document.getElementById("cityBtn");
+    var bothBtn = document.getElementById("bothBtn");
+    var airlineBtn = document.getElementById("airlineBtn");
+    airlineBtn.style.backgroundColor = "#e7e7e7";
+    cityBtn.style.backgroundColor = "#e7e7e7";
+    bothBtn.style.backgroundColor = "#00BFFF";
+
+    var bothDiv = document.getElementById("Both");
+    bothDiv.style.display = "block";
+
+    var airlines = Object.keys(airlineMap).sort();
+
+    var index = airlines.indexOf("All");
+    if(index > -1){
+        airlines.splice(index,1);
+    }
+
+    var bothDrop = d3.select("#Both")
+                    .append("select")
+                    .attr("padding", 30)
+                    .on("change", function(){
+                        var newKey = d3.select(this).property('value'),
+                        cityMap2 = tierdMap[newKey];
+                        newMaxVal = tierdMaxVal[newKey];
+
+                        var container1 = d3.select("#vis-container1");
+                        var container2 = d3.select("#vis-container2");
+
+                        makeVis(container1, cityMap2, newMaxVal);
+                        makeVis(container2, cityMap2, newMaxVal);
+                    });
+
+    bothDrop
+    .selectAll("option")
+    .data(airlines)
+    .enter().append("option")
+    .attr("value", function (d) { return d; })
+    .property("selected", function(d){ return d === "American"; })
+    .text(function (d) {
+        return d; 
+    }); 
+
+
+    var cityMap2 = tierdMap["American"];
+    var newMaxVal = tierdMaxVal["American"];
+
+    var container1 = d3.select("#vis-container1");
+    var container2 = d3.select("#vis-container2");
+
+    makeVis(container1, cityMap2, newMaxVal);
+    makeVis(container2, cityMap2, newMaxVal);
+}
+
+var makeVis = function(container, map, vals) {
+
+    container.selectAll('*').remove();
+    // Define dimensions of vis
+    var margin = { top: 30, right: 50, bottom: 30, left: 50 },
+        width  = 600 - margin.left - margin.right,
+        height = 450 - margin.top  - margin.bottom;
+
+        // Make x scale
+    var xScale = d3.scale.ordinal()
+        .domain(chartLables)
+        .rangeBands([0, width],0.1);
+
+    var yScale = d3.scale.linear()
+        .range([height, 0]);
+
+
+    var svg = container
+        .append("svg")
+        .attr("width",  width  + margin.left + margin.right)
+        .attr("height", height + margin.top  + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + (margin.top) + ")");
+
+    function transform_tick(x){
+        return x/6 + ":00";
+    }
+
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom")
+        .tickValues([0,12,24,36,48,60,72,84,96,108,120,132])
+        .tickFormat(function(d){ return transform_tick(d); });
+
+    var xAxisElem = svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+
+    svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width+26)
+    .attr("y", height)
+    .text("Time");
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left");
+
+    var yAxisHandleForUpdate = svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    yAxisHandleForUpdate.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Value");
+
+    var label = svg.append("text")
+        .attr("class", "compare label")
+        .attr("text-anchor", "middle")
+        .attr("y", 0)
+        .attr("x", width/2)
+        .text(1);
+
+    var description = svg.append("text")
+        .attr("class", "compare text")
+        .attr("text-anchor", "start")
+        .attr("y", 30)
+        .attr("x", width/2)
+        .text(1);
+
+    var updateBars = function(data, max, curKey) {
+        // First update the y-axis domain to match data
+        yScale.domain( [0, d3.max(data, function(d) { return d;})]);
+        yAxisHandleForUpdate.call(yAxis);
+
+        label.text(curKey);
+
+        var max_time = 0;
+        for( var i=0; i< data.length; i++){
+            if(data[i] === max){
+                max_time = i;
+                break;
+            }
+        }
+
+        function pad_digit(x){
+            if(x < 10){
+                return "0"+x;
+            } else{
+                return x;
+            }
+        }
+
+        show_time = pad_digit(Math.floor(max_time * 10 / 60)) + ":" + pad_digit(max_time * 10 % 60);
+        description.text("Peak at " +show_time + " of " + Math.round(max *100)/100 + " flights");
+
+        var bars = svg.selectAll(".bar").data(data);
+           
+        //remove original charts
+        bars.transition()
+            .duration(200)
+            .attr("height", function(d,i) { return 0; });
+
+        //add new charts
+        bars.enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d,i) { return xScale( chartLables[i] ); })
+            .attr("width", xScale.rangeBand())
+            .attr("y", function(d,i) { 
+                return yScale(d); 
+            })
+            .attr("height", 0);
+
+        //make animation
+
+        bars
+            .transition()
+            .duration(200)
+            .delay(function (d, i) {
+                return i * 10;
+            })
+            .attr("y", function(d) { return yScale(d); })
+            .attr("height", function(d,i) { 
+                return height - yScale(d); 
+            });
+
+        bars.attr("class", "bar")
+            .filter(function(d) { 
+                return d === max; 
+            })
+            .classed("peak_bar", true);
+
+        bars.exit().remove();
+
+    };
+
+    var dropdownChange = function() {
+        var newKey = d3.select(this).property('value'),
+            newData   = map[newKey],
+            max = vals[newKey];
+        updateBars(newData, max, newKey);
+    };
+
+    var dropdown = container.insert("select", "svg")
+                            .attr("align", "center")
+                            .on("change", dropdownChange);
+
+    var cities = Object.keys(map).sort();
+
+    dropdown.selectAll("option")
+        .data(cities)
+        .enter().append("option")
+        .attr("value", function (d) { return d; })
+        .property("selected", function(d){ return d === "All"; })
+        .text(function (d) {
+            return d; // capitalize 1st letter
+        });
+
+    var initialData = map["All"];
+    updateBars(initialData, vals.All, "All");
+};
---
+++ js/delay.js
@@ -0,0 +1,241 @@
+function x(d) { return d.avg_delay_time; }
+function y(d) { return d.delay_rate; }
+function radius(d) { return d.total; }
+function color(d) { return d.name; }
+function key(d) { return d.name; }
+
+var airline_map = {"NK":"Spirit",
+                   "WN":"Southwest",
+                   "B6":"Jetblue",
+                   "UA":"United",
+                   "AA":"American",
+                   "DL":"Delta",
+                   "OO":"SkyWest",
+                   "F9":"Frontier",
+                   "EV":"ExpressJet",
+                   "HA":"Hawaiian",
+                   "AS":"Alaska",
+                   "MQ":"AmericanEagle"
+                  };
+
+var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 29.5},
+    width = 750 - margin.right-margin.left,
+    height = 500 - margin.top - margin.bottom;
+
+var xScale = d3.scale.pow().exponent(0.7).domain([0,50]).range([0, width]),
+    yScale = d3.scale.linear().domain([0, 0.8]).range([height, 0]),
+    radiusScale = d3.scale.sqrt().domain([0, 3000]).range([0, 20]),
+    colorScale = d3.scale.category20();
+
+var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(10),
+    yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(10);
+
+var svg = d3.select("#chart-delay").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .style("background-color","white")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height - 6)
+    .text("average delay time (minutes)");
+
+svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("delay rate");
+
+var label = svg.append("text")
+    .attr("class", "date label")
+    .attr("text-anchor", "end")
+    .attr("y", height - 24)
+    .attr("x", width)
+    .text(1);
+
+var div = d3.select("body").append("div") 
+    .attr("class", "delaytooltip")       
+    .style("opacity", 0);
+
+function start(){
+
+  var elem = document.getElementById('start_button');
+  elem.parentNode.removeChild(elem);
+
+  d3.json("ReadyData/airline_delay.json", function(flights) {
+
+    var bisect = d3.bisector(function(d) { return d[0]; });
+
+    function make_percent(x){
+      return Math.trunc(x*100) + "%";
+    }
+
+    function round_twodigits(x){
+      return Math.round(x * 100) / 100
+    }
+
+    var dot = svg.append("g")
+        .selectAll(".dot")
+        .data(interpolateData(1))
+        .enter().append("circle")
+        .style("fill", function(d) { return colorScale(radius(d)); })
+        .call(position)
+        .sort(order)
+        .on("mouseover", function(d) {    
+            div.transition()    
+                .duration(200)    
+                .style("opacity", .9);    
+            div .html("Airline:&nbsp&nbsp" + airline_map[d.name] + "<br/>"  + "Avg_delay:&nbsp" + round_twodigits(d.avg_delay_time) +" min" 
+              + "<br/>" + "Delay_rate:&nbsp&nbsp"+make_percent(d.delay_rate) + "<br/>Flights:&nbsp&nbsp" + Math.trunc(d.total)) 
+                .style("left", (d3.event.pageX) + "px")   
+                .style("top", (d3.event.pageY) + "px");  
+            })          
+        .on("mouseout", function(d) {   
+            div.transition()    
+                .duration(500)    
+                .style("opacity", 0); 
+        });
+
+    var box = label.node().getBBox();
+
+    var overlay = svg.append("rect")
+          .attr("class", "overlay")
+          .attr("x", box.x - 4.5* box.width)
+          .attr("y", box.y+box.height/3)
+          .attr("width", 5.5* box.width)
+          .attr("height", box.height/3)
+          .on("mouseover", enableInteraction);
+
+    svg.transition()
+        .duration(20000)
+        .ease("linear")
+        .tween("date", tweenDate)
+        .each("end", enableInteraction);
+
+    // Positions the dots based on data.
+    function position(dot) {
+      dot .attr("cx", function(d) { return xScale(x(d)); })
+          .attr("cy", function(d) { return yScale(y(d)); })
+          .attr("r", function(d) { 
+            return radiusScale(radius(d)); 
+          });
+    }
+
+    // Defines a sort order so that the smallest dots are drawn on top.
+    function order(a, b) {
+      return radius(b) - radius(a);
+    }
+
+    function enableInteraction() {
+      var dateScale = d3.scale.linear()
+          .domain([1, 53])
+          .range([box.x - 4.5* box.width+5, box.x + box.width-5 ])
+          .clamp(true);
+
+      svg.transition().duration(0);
+
+      overlay
+          .on("mouseover", mouseover)
+          .on("mouseout", mouseout)
+          .on("mousemove", mousemove)
+          .on("touchmove", mousemove);
+
+      function mouseover() {
+        label.classed("active", true);
+      }
+
+      function mouseout() {
+        label.classed("active", false);
+      }
+
+      function mousemove() {
+        displayDate(dateScale.invert(d3.mouse(this)[0]));
+      }
+    }
+
+
+    function tweenDate() {
+      var date = d3.interpolateNumber(1, 53);
+      return function(t) { displayDate(date(t)); };
+    }
+
+    function displayDate(date) {
+      dot.data(interpolateData(date)).call(position).sort(order);
+      var day = (Math.round(date)-1)*7+1;
+      label.text(convert_date(day));
+    }
+
+    function convert_date(date){
+      if(date <=31){
+        return "Jan." + padSingleDigit(date);
+      } else if( date <=59){
+        return "Feb." + padSingleDigit(date -31);
+      } else if(date <= 90){
+        return "Mar." + padSingleDigit(date - 59);
+      } else if(date <= 120){
+        return "Apr." + padSingleDigit(date - 90);
+      } else if(date <= 151){
+        return "May." + padSingleDigit(date -120);
+      } else if(date <= 181){
+        return "Jun." + padSingleDigit(date - 151);
+      } else if(date <= 212){
+        return "Jul." + padSingleDigit(date - 181);
+      } else if(date <= 243){
+        return "Aug." + padSingleDigit(date -212);
+      } else if(date <= 273){
+        return "Sep." + padSingleDigit(date -243);
+      } else if(date <= 304){
+        return "Oct." + padSingleDigit(date - 273);
+      } else if(date <= 334){
+        return "Nov." + padSingleDigit(date - 304);
+      } else{
+        return "Dec." + padSingleDigit(date - 334);
+      }
+    }
+
+    function padSingleDigit(x){
+      if(x < 10){
+        return "0"+x;
+      } else{
+        return x;
+      }
+    }
+
+    function interpolateData(date) {
+      return flights.map(function(d) {
+        return {
+          name: d.name,
+          delay_rate: interpolateValues(d.delay_rate, date),
+          total: interpolateValues(d.total, date),
+          avg_delay_time: interpolateValues(d.avg_delay_time, date)
+        };
+      });
+    }
+
+    function interpolateValues(values, date) {
+      var i = bisect.left(values, date, 0, values.length - 1),
+          a = values[i];
+      if (i > 0) {
+        var b = values[i - 1],
+            t = (date - a[0]) / (b[0] - a[0]);
+        return a[1] * (1 - t) + b[1] * t;
+      }
+      return a[1];
+    }
+  });
+}
